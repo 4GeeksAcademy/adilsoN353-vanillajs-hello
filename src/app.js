444cf79b0656057bdd0a3b204257e478ed5e4349
adilson aguilar <163350523+adilsoN353@users.noreply.github.com>
2024-04-15 18:48:41 +0000

    generador de excusas terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,37 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generatorexcuse();
+    //write your code here
+  });
+  let randomnumber = Math.random() * 10;
+
+  let generatorexcuse = () => {
+    let who = ["El perro", "Mi ", "El cartero", "Mi pajaro"];
+    let action = ["comio", "orinar", "aplastada", "rompio"];
+    let what = ["mi tarea", "mi telefono", "el coche"];
+    let when = [
+      "despues de clases",
+      "cuando iba a dormir",
+      "mientras hacia ejercicio",
+      "durante el ealmuerzo",
+      "mientras estaba orando"
+    ];
+
+    let whoindex = Math.floor(Math.random() * who.length);
+    let actionindex = Math.floor(Math.random() * action.length);
+    let whatindex = Math.floor(Math.random() * what.length);
+    let whenindex = Math.floor(Math.random() * when.length);
+
+    return (
+      who[whoindex] +
+      " " +
+      action[actionindex] +
+      " " +
+      what[whatindex] +
+      " " +
+      when[whenindex]
+    );
+  };
 };
