c5dca633f184d511c11b5f1fd175394bae3c58ac
adilson aguilar <163350523+adilsoN353@users.noreply.github.com>
2024-05-23 20:34:58 +0000

    cambios de buenas practicas de generador de excusas

--- src/app.js
+++ src/app.js
@@ -13,30 +13,151 @@ window.onload = function() {
   let randomnumber = Math.random() * 10;
 
   let generatorexcuse = () => {
-    let who = ["El perro", "Mi ", "El cartero", "Mi pajaro"];
-    let action = ["comio", "orinar", "aplastada", "rompio"];
-    let what = ["mi tarea", "mi telefono", "el coche"];
-    let when = [
-      "despues de clases",
-      "cuando iba a dormir",
-      "mientras hacia ejercicio",
-      "durante el ealmuerzo",
-      "mientras estaba orando"
+    const WHO = [
+      "El perro",
+      "Mi gato",
+      "El cartero",
+      "Mi pájaro",
+      "El vecino",
+      "Un extraterrestre",
+      "Mi abuela",
+      "El fantasma",
+      "Un payaso",
+      "Mi hermano",
+      "La policía",
+      "Un ladrón",
+      "El presidente",
+      "Un mago",
+      "Mi profesor",
+      "El jardinero",
+      "Un dragón",
+      "La televisión",
+      "El chef",
+      "El mecánico",
+      "Un robot",
+      "El electricista",
+      "Un conejo",
+      "Mi tío",
+      "Una cabra",
+      "El fontanero",
+      "El bombero",
+      "El cocodrilo",
+      "Un oso",
+      "El ratón"
     ];
 
-    let whoindex = Math.floor(Math.random() * who.length);
-    let actionindex = Math.floor(Math.random() * action.length);
-    let whatindex = Math.floor(Math.random() * what.length);
-    let whenindex = Math.floor(Math.random() * when.length);
+    const ACTION = [
+      "comió",
+      "orinó en",
+      "aplastó",
+      "rompió",
+      "robó",
+      "quemó",
+      "escondió",
+      "perdió",
+      "destruyó",
+      "pintó",
+      "mordió",
+      "escupió",
+      "rayó",
+      "arrancó",
+      "ensució",
+      "sabotajeó",
+      "se deshizo de",
+      "vandalizó",
+      "perforó",
+      "voló",
+      "manchó",
+      "echó a perder",
+      "derrumbó",
+      "aplastó",
+      "reventó",
+      "tritó",
+      "rasgó",
+      "empapó",
+      "desconfiguró",
+      "zumbó"
+    ];
+
+    const WHAT = [
+      "mi tarea",
+      "mi teléfono",
+      "el coche",
+      "mi computadora",
+      "el libro",
+      "la ropa",
+      "los deberes",
+      "la bicicleta",
+      "el proyecto",
+      "la pizza",
+      "el informe",
+      "el ensayo",
+      "la cámara",
+      "el pastel",
+      "los zapatos",
+      "el reloj",
+      "la televisión",
+      "el regalo",
+      "el juguete",
+      "la planta",
+      "el sofá",
+      "la lámpara",
+      "la puerta",
+      "la ventana",
+      "el teclado",
+      "el ratón",
+      "el escritorio",
+      "la mochila",
+      "la guitarra",
+      "el piano"
+    ];
+
+    const WHEN = [
+      "después de clases",
+      "cuando iba a dormir",
+      "mientras hacía ejercicio",
+      "durante el almuerzo",
+      "mientras estaba orando",
+      "en la madrugada",
+      "en medio de la noche",
+      "cuando estaba en el baño",
+      "en la fiesta",
+      "durante el examen",
+      "en el cumpleaños de mi amigo",
+      "cuando jugaba videojuegos",
+      "mientras cocinaba",
+      "cuando paseaba al perro",
+      "en el parque",
+      "cuando estaba estudiando",
+      "en la biblioteca",
+      "durante el viaje",
+      "mientras estaba en el trabajo",
+      "cuando limpiaba la casa",
+      "en la piscina",
+      "cuando estaba nadando",
+      "durante la lluvia",
+      "mientras hablaba por teléfono",
+      "cuando estaba comprando",
+      "durante la película",
+      "cuando estaba en clase",
+      "mientras conducía",
+      "en la boda",
+      "durante la siesta"
+    ];
+
+    let whoindex = Math.floor(Math.random() * WHO.length);
+    let actionindex = Math.floor(Math.random() * ACTION.length);
+    let whatindex = Math.floor(Math.random() * WHAT.length);
+    let whenindex = Math.floor(Math.random() * WHEN.length);
 
     return (
-      who[whoindex] +
+      WHO[whoindex] +
       " " +
-      action[actionindex] +
+      ACTION[actionindex] +
       " " +
-      what[whatindex] +
+      WHAT[whatindex] +
       " " +
-      when[whenindex]
+      WHEN[whenindex]
     );
   };
 };
